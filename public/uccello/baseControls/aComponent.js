86c9dfae9becd070ed9fe82af451a007fb9d5a75
IvanCollector <ivan.collector@gmail.com>
2014-10-16 16:51:57 +0400

    rrrr

--- public/uccello/baseControls/aComponent.js
+++ public/uccello/baseControls/aComponent.js
@@ -9,7 +9,6 @@ define(
         var AComponent = Class.extend({
 		
 			className: "AComponent",
-<<<<<<< HEAD
 			classGuid: "5b8c93e7-350d-de2a-e2b4-1025a03b17db",
 			metaFields: [ {fname:"Id",ftype:"int"}, {fname:"Name",ftype:"string"} ], // TODO? гуиды для полей?
 	
@@ -17,26 +16,12 @@ define(
 				this._buildMetaInfo(db);
             },
 			
-=======
-			classGuid: "",
-		
-
-
-            init: function(){
-				
-                //this.typeName = "Control";
-            }
-        }, {
-
-            metaFields: [ {fname:"Id",ftype:"int"}, {fname:"Name",ftype:"string"} ],
-
->>>>>>> origin/master
             /**
              * Cоздает метаинформацию своего класса в базе данных db
              * @static
              * @param db
              */
-            _buildMetaInfo: function(db){
+            _buildMetaInfo: function(db){ 
                 if (!db.getObj(this.classGuid)) {
 					var obj = Object.getPrototypeOf(this), pobj=null;
 					if (obj.className != "AComponent")
