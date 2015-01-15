dbcb5c18f55a84d2badc05ce04a9a19a49046b35
Sergey <rudserg@gmail.com>
2015-01-15 13:07:30 +0300

    EditData

---
+++ public/uccello/controls/aDataFieldControl.js
@@ -0,0 +1,21 @@
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+    var Class = require('class.extend');
+}
+
+define(
+    ['./aDataControl'],
+    function(ADataControl) {
+        var ADataFieldControl = ADataControl.extend({
+
+            className: "ADataFieldControl",
+            classGuid: "00a12976-6fe3-6592-1984-635684b30885",
+            metaFields: [{fname: "DataField", ftype: "string"}],
+
+            init: function(cm,params){
+                this._super(cm,params);
+            }
+        });
+        return ADataFieldControl;
+    }
+);
