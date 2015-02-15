2f28d8048efadb3574850f7fd691bcde70c8b291
IvanCollector <ivan.collector@gmail.com>
2015-02-15 19:07:43 +0300

    modifParam step1

--- public/uccello/controls/formParam.js
+++ public/uccello/controls/formParam.js
@@ -13,12 +13,33 @@ define(
             metaFields: [
                 {fname: "Type", ftype: "string"},
                 {fname: "Kind", ftype: "string"},
-                {fname: "Value", ftype: "string"}
+                {fname: "Value", ftype: "string"},
+				{fname: "OnModify", ftype: "event"}
             ],
 
             init: function(cm,params){
                 this._super(cm,params);
+				
+				// примерчик: приклеиваем обработчик события onModify
+				if (this.getObj()) {
+					if (this.getObj().get("OnModify")) {
+						this.onModify = new Function("newVal",this.getObj().get("OnModify"));
+					}
+				}
+				
             },
+			
+			processDelta: function() {
+				var obj = this.getObj();
+				if (obj.isFldModified("Value") && ( "onModify" in this)) {
+					this.onModify(this.value());
+					//apply(
+					/*this.event.fire({
+						type: 'modParam',
+						target: this				
+					});*/
+				}
+			},
 
             type: function (value) {
                 return this._genericSetter("Type", value);
@@ -31,6 +52,7 @@ define(
             value: function (value) {
                 return this._genericSetter("Value", value);
             }
+			
         });
         return FormParam;
     }
