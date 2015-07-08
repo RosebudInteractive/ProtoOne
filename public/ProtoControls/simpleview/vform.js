6d6eb04959328bbc38dd74a8b21aa5b1d7d1fd37
Sergey <rudserg@gmail.com>
2015-07-08 17:46:17 +0300

    empty form

--- public/ProtoControls/simpleview/vform.js
+++ public/ProtoControls/simpleview/vform.js
@@ -6,21 +6,23 @@ define(
         vForm.render = function(options) {
             var item = $('#' + this.getLid());
             if (item.length == 0) {
+                // объект контейнера
                 item = $(vForm._templates['form']).attr('id', this.getLid());
 
-                // создаем врапперы для чайлдов
-                var childs = this.getCol('Children');
-                for(var i=0; i<childs.count();i++) {
-                    var child = this.getControlMgr().get(childs.get(i).getGuid());
-                    if (!child.left) continue;
-                    var div = $('<div class="control-wrapper"></div>').attr('id', 'ch_'+child.getLid());
-                    var height=child.height();
-                    div.css({flex: height+' 0 auto',  '-webkit-flex': height+' 0 auto', '-ms-flex': height+' 0 auto'});
-                    item.append(div);
-                }
-
-                var parent = this.getParent()? '#' + this.getLid(): options.rootContainer;
+                // добавляем в парент
+                var parent = this.getParent()? '#ch_' + this.getLid(): options.rootContainer;
                 $(parent).append(item);
+            }
+
+            // создаем врапперы для чайлдов
+            var childs = this.getCol('Children');
+            for(var i=0; i<childs.count();i++) {
+                var child = this.getControlMgr().get(childs.get(i).getGuid());
+                if (!child.left) continue;
+                var div = $('<div class="control-wrapper"></div>').attr('id', 'ch_'+child.getLid());
+                var height=child.height();
+                div.css({flex: height+' 0 auto',  '-webkit-flex': height+' 0 auto', '-ms-flex': height+' 0 auto'});
+                item.append(div);
             }
 
             // убираем удаленные объекты
