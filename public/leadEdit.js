f7060a7db754c20733d228e168476d1f221a2878
Sergey <rudserg@gmail.com>
2015-08-10 14:29:36 +0300

    add userEventHandler

--- public/leadEdit.js
+++ public/leadEdit.js
@@ -80,21 +80,23 @@ define(
             
             setEditForm: function(edit) {
                 var cm = this.pvt.uccelloClt.getContextCM();
-                cm.getByName('EditButton').enabled(!edit);
-                cm.getByName('ConvertButton').enabled(!edit);
-                cm.getByName('ArchiveButton').enabled(!edit);
-                cm.getByName('NextButton').enabled(!edit);
-                cm.getByName('PrevButton').enabled(!edit);
-                cm.getByName('SaveButton').enabled(edit);
-                cm.getByName('CancelButton').enabled(edit);
-                cm.getByName('NewButton').enabled(!edit);
-                cm.getByName('LeadId').enabled(edit);
-                cm.getByName('LeadSource').enabled(edit);
-                cm.getByName('LeadState').enabled(edit);
-                cm.getByName('LeadContent').enabled(edit);
-                cm.getByName('LeadCreation').enabled(edit);
-                cm.getByName('LeadClosed').enabled(edit);
-                cm.getByName('LeadOpportunityId').enabled(edit);
+                cm.userEventHandler(this, function () {
+                    cm.getByName('EditButton').enabled(!edit);
+                    cm.getByName('ConvertButton').enabled(!edit);
+                    cm.getByName('ArchiveButton').enabled(!edit);
+                    cm.getByName('NextButton').enabled(!edit);
+                    cm.getByName('PrevButton').enabled(!edit);
+                    cm.getByName('SaveButton').enabled(edit);
+                    cm.getByName('CancelButton').enabled(edit);
+                    cm.getByName('NewButton').enabled(!edit);
+                    cm.getByName('LeadId').enabled(edit);
+                    cm.getByName('LeadSource').enabled(edit);
+                    cm.getByName('LeadState').enabled(edit);
+                    cm.getByName('LeadContent').enabled(edit);
+                    cm.getByName('LeadCreation').enabled(edit);
+                    cm.getByName('LeadClosed').enabled(edit);
+                    cm.getByName('LeadOpportunityId').enabled(edit);
+                });
             },
 
             _dataset: function () {
