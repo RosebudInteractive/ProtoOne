edfb2855bffcd13e46798afc8f47381f12899c9a
Sergey <rudserg@gmail.com>
2015-10-05 19:17:49 +0300

    root res

--- public/proto1.js
+++ public/proto1.js
@@ -1055,7 +1055,7 @@ $(document).ready( function() {
                 var keyCode = e.keyCode || e.which, control;
                 if (keyCode == 9) {
                     e.preventDefault();
-                    var form = uccelloClt.getContextCM().getByName('MainForm');
+                    var cm=uccelloClt.getContextCM(), form = cm.getRoot(cm.getRootGuids("res")[0]).obj;
                     var focusControl = form.currentControl()?form.currentControl():form;
                     if (e.shiftKey) {
                         control = focusControl.prev(true);
