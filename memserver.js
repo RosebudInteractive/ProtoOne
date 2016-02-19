caeb8ea20fb446797af857b7369892f7e2b33236
SashaSokolov <alexander.f.sokolov@gmail.com>
2016-02-19 09:55:48 +0300

    Default settings.

--- memserver.js
+++ memserver.js
@@ -155,40 +155,40 @@ var config = {
         //        idle: 10000
         //    },
         //},
-        connection: { //MySql
-            host: "localhost",
-            username: "root",
-            password: "masterkey",
-            database: "uccello",
-            provider: "mysql",
-            connection_options: {},
-            provider_options: {},
-            pool: {
-                max: 5,
-                min: 0,
-                idle: 10000
-            },
-        },
-        importData: {
-            autoimport: false,
-            dir: "./data/tables"
-        },
+        //connection: { //MySql
+        //    host: "localhost",
+        //    username: "sa",
+        //    password: "system",
+        //    database: "genetix_test",
+        //    provider: "mysql",
+        //    connection_options: {},
+        //    provider_options: {},
+        //    pool: {
+        //        max: 5,
+        //        min: 0,
+        //        idle: 10000
+        //    },
+        //},
+        //importData: {
+        //    autoimport: false,
+        //    dir: "./data/tables"
+        //},
         trace: {
             sqlCommands: true,
             importDir: true
         }
     },
     resman: {
-        useDb: false,
+        //useDb: true,
         defaultProduct : "ProtoOne"
     },
-    resourceBuilder : {
-        sourceDir : "./data/forms/",
-        destDir : "./data/tables/",
-        formResTypeId : 1,
-        productId : 2,
-        currBuildId : 2
-    }
+    //resourceBuilder : {
+    //    sourceDir : "./data/forms/",
+    //    destDir : "./data/tables/",
+    //    formResTypeId : 1,
+    //    productId : 2,
+    //    currBuildId : 2
+    //}
 };
 
 // модуль настроек
