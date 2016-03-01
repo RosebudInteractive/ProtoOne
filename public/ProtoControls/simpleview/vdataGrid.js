1abc28e9d42a5861e19ea57da02d316a6574801c
SashaSokolov <alexander.f.sokolov@gmail.com>
2016-03-01 10:22:19 +0300

    Using "ds.canMoveCursor" in "click" event processing.

--- public/ProtoControls/simpleview/vdataGrid.js
+++ public/ProtoControls/simpleview/vdataGrid.js
@@ -45,9 +45,9 @@ define(
                         var cursor = rowTr.attr('data-id');
                         var ds = that.dataset();
                         var dsState = !ds ? Meta.State.Unknown : ds.getState();
-                        if (ds.cursor() != cursor &&
+                        if (ds.cursor() != cursor && ds.canMoveCursor()/*&&
                             (ds.getState() == Meta.State.Browse ||
-                            (dsState != Meta.State.Unknown && dsState != Meta.State.Pending && ds.cachedUpdates()))) {
+                            (dsState != Meta.State.Unknown && dsState != Meta.State.Pending && ds.cachedUpdates()))*/) {
                             vDataGrid.renderCursor.apply(that, [cursor]);
                             that.getControlMgr().userEventHandler(that, function(){
                                 that.dataset().cursor(cursor);
