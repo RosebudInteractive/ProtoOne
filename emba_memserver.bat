ca66a2c99eacbf839b19c70209eadb482c4d2d7b
SashaSokolov <alexander.f.sokolov@gmail.com>
2016-07-28 09:42:38 +0300

    Параметры запуска для host-а EMBA.

---
+++ emba_memserver.bat
@@ -0,0 +1,12 @@
+set DB_TYPE=ms_sql
+set MS_HOST=10.1.1.3
+set MS_USER=sa
+set MS_PASSWORD=system
+set MS_DB=genetix_test_sokolov
+set MS_INST=SQL2008R2
+set MS_PORT=1435
+set MY_HOST=localhost
+set MY_USER=sa
+set MY_PASSWORD=system
+set MY_DB=genetix_test
+node memserver.js
